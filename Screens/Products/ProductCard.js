1247491799751034a28297f5776266bdde1d97cc
theo82 <theoaristi53@gmail.com>
2021-04-07 11:39:35 +0300

    Can not get values in Product Card - Error

---
+++ Screens/Products/ProductCard.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { 
+    View, 
+    Text, 
+    StyleSheet, 
+    Image, 
+    Button, 
+    Dimensions
+} from 'react-native';
+
+const { width } = Dimensions.get("window");
+
+export const ProductCard = (props) => {
+    const { name, price, image, countInStock } = props;
+
+    return (
+        <View style={styles.container}>
+            <Image style={styles.image}/>
+            <View style={styles.card}/>
+            <Text styles={styles.title}>
+                {name.length > 15 ? name.substring(0, 15 - 3)
+                    + '...' : name
+                }
+            </Text>
+            <Text style={styles.price}>${price}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: width / 2 - 20,
+        height: width / 1.7,
+        borderRadius: 10,
+        marginTop: 55,
+        marginBottom: 5,
+        marginLeft: 10,
+        alignItems: 'center',
+        elevation: 8,
+        backgroundColor: 'white',
+    },
+    image: {
+            width: width / 2 - 20 - 10,
+            height: width / 2 - 20 - 30,
+            backgroundColor: 'transparent',
+            position: 'absolute',
+            top: -45
+    },
+    card: {
+        marginBottom: 10,
+        height: width / 2 - 20 - 90,
+        backgroundColor: 'transparent',
+        width: width / 2 - 20 - 10
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 14,
+        textAlign: 'center'
+    },
+    price: {
+        fontSize: 20,
+        color: 'orange',
+        marginTop: 10
+    }
+})
+
+export default ProductCard;
