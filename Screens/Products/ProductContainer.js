2f80cb2e99b90e9aa1df638b89a564973649f905
theo82 <theoaristi53@gmail.com>
2021-04-07 08:59:21 +0300

    Intro And Forder Structure

---
+++ Screens/Products/ProductContainer.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+
+const ProductContainer = () => {
+    return (
+        <View>
+            <Text>Product Container Screen</Text>
+        </View>
+    )
+}
+
+export default ProductContainer;
