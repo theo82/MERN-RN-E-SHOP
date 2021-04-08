a57f2429738082bb9f8c1372c8d413c907bb91ee
theo82 <theoaristi53@gmail.com>
2021-04-08 08:06:02 +0300

    Native Base

--- Screens/Products/ProductContainer.js
+++ Screens/Products/ProductContainer.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, ActivityIndicator, FlatList } from 'react-native';
+import { View, StyleSheet, ActivityIndicator, FlatList } from 'react-native';
+import { Container, Header, Icon, Item, Input, Text } from 'native-base';
 
 const data = require('../../assets/data/products.json');
 
@@ -17,24 +18,51 @@ const ProductContainer = () => {
     }, [])
 
     return (
-        <View>
-            <View>
-                <Text>Product Container Screen</Text>
-            </View>
-            <View>
-                <FlatList 
-                    numColumns={2}
-                    keyExtractor = {(item) => item.name}
-                    data = {products}
-                    renderItem = {({item}) => <ProductList 
-                        key={item.id} 
-                        item={item}
+        <Container>
+            <Header searchBar rounded >
+                <Item>
+                    <Icon name="ios-search"/>
+                    <Input 
+                        placeholder="Search"
+                        // onFocus={}
+                        // onChangeText = {(text) => }
+
+                        
                     />
-                    }
-                />
+                </Item>
+            </Header>
+            
+            <View style={styles.container}>
+                    <Text>Product Container Screen</Text>
+                <View style={styles.listContainer}>
+                    <FlatList 
+                        numColumns={2}
+                        keyExtractor = {(item) => item.name}
+                        data = {products}
+                        renderItem = {({item}) => <ProductList 
+                            key={item.id} 
+                            item={item}
+                        />
+                        }
+                    />
+                </View>
             </View>
-        </View>
+        </Container>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flexWrap: "wrap",
+      backgroundColor: "gainsboro",
+    },
+    listContainer: {
+      flex: 1,
+      flexDirection: "row",
+      alignItems: "flex-start",
+      flexWrap: "wrap",
+      backgroundColor: "gainsboro",
+    }
+  });
 
 export default ProductContainer;
