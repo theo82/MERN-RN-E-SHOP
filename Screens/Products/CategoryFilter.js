009b967d6ba152b8b889e934cd3156289c01a508
theo82 <theoaristi53@gmail.com>
2021-04-12 09:45:45 +0300

    Installing React Navigation

--- Screens/Products/CategoryFilter.js
+++ Screens/Products/CategoryFilter.js
@@ -28,9 +28,9 @@ const CategoryFilter = (props) => {
         </TouchableOpacity>
         {props.categories.map((item) => (
           <TouchableOpacity
-            key={item._id.$oid}
+            key={item._id}
             onPress={() => {
-              props.categoryFilter(item._id.$iod),
+              props.categoryFilter(item._id),
                 props.setActive(props.categories.indexOf(item));
             }}
           >
