6e82c8ba1a853e26b769cb54cb3bfe4de591d292
theo82 <theoaristi53@gmail.com>
2021-04-12 17:06:33 +0300

    Bottom Tab Navigation

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, LogBox, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+// Navigators
+import Main from './Navigators/Main';
 
 //Screens
 import ProductContainer from './Screens/Products/ProductContainer';
@@ -10,18 +14,11 @@ LogBox.ignoreAllLogs(true);
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <NavigationContainer>
       <Header />
-      <ProductContainer />
-    </View>
+      <Main />
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
