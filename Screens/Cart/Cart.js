7b6f7d739bfc067362bf50aeeac5bc8e60d1693f
theo82 <theoaristi53@gmail.com>
2021-04-16 13:39:30 +0300

    Cart Item

--- Screens/Cart/Cart.js
+++ Screens/Cart/Cart.js
@@ -1,14 +1,60 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import { connect } from 'react-redux';
+import { View, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
 
+import {
+  Container,
+  Text,
+  Left,
+  Right,
+  H1,
+  ListItem,
+  Thumbnail,
+  Body,
+} from 'native-base';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import { connect } from 'react-redux';
+import * as actions from '../..//Redux/Actions/cartActions';
+
+var { height, width } = Dimensions.get('window');
 const Cart = (props) => {
   return (
-    <View style={{ flex: 1 }}>
-      {props.cartItems.map((x) => {
-        return <Text>{x.product.name}</Text>;
-      })}
-    </View>
+    <>
+      {props.cartItems.length ? (
+        <Container>
+          <H1 style={{ alignSelf: 'center' }}>Cart</H1>
+          {props.cartItems.map((data) => {
+            return (
+              <ListItem style={styles.listItem} key={Math.random()} avatar>
+                <Left>
+                  <Thumbnail
+                    source={{
+                      uri: data.product.image
+                        ? data.product.image
+                        : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png',
+                    }}
+                  />
+                </Left>
+                <Body style={styles.body}>
+                  <Left>
+                    <Text>{data.product.name}</Text>
+                  </Left>
+                  <Right>
+                    <Text>${data.product.price}</Text>
+                  </Right>
+                </Body>
+              </ListItem>
+            );
+          })}
+        </Container>
+      ) : (
+        <Container style={styles.emptyContainer}>
+          <Text>Looks like your cart is empty</Text>
+          <Text>Add products to your cart to get started</Text>
+        </Container>
+      )}
+    </>
   );
 };
 
@@ -18,5 +64,23 @@ const mapStateToProps = (state) => {
     cartItems: cartItems,
   };
 };
+
+const styles = StyleSheet.create({
+  emptyContainer: {
+    height: height,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  listItem: {
+    alignItems: 'center',
+    backgroundColor: 'white',
+    justifyContent: 'center',
+  },
+  body: {
+    margin: 10,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+});
 
 export default connect(mapStateToProps, null)(Cart);
