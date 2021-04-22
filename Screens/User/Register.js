25edc7716ae049b8a2f55c75a60070f7f21681d3
theo82 <theoaristi53@gmail.com>
2021-04-22 20:09:25 +0300

    Toast

--- Screens/User/Register.js
+++ Screens/User/Register.js
@@ -4,6 +4,8 @@ import FormContainer from '../../Shared/Form/FormContainer';
 import Input from '../../Shared/Form/Input';
 import Error from '../../Shared/Error';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import Toast from 'react-native-toast-message';
+
 import axios from 'axios';
 import baseURL from '../../assets/common/baseUrl';
 
@@ -31,12 +33,24 @@ const Register = (props) => {
       .post(`${baseURL}users/register`, user)
       .then((res) => {
         if (res.status === 200) {
+          Toast.show({
+            type: 'success',
+            text1: 'Registration Succeded',
+            text2: ' Please into your acccount',
+          });
           setTimeout(() => {
             props.navigation.navigate('Login');
           }, 500);
         }
       })
-      .catch((error) => {});
+      .catch((error) => {
+        Toast.show({
+          topOffset: 60,
+          type: 'error',
+          text1: 'Something went wrong',
+          text2: 'Please try again',
+        });
+      });
   };
 
   return (
