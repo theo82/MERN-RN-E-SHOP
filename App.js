a4c5fa152492755cb11ca6c40234dc2c8c4ffb38
theo82 <theoaristi53@gmail.com>
2021-04-24 10:28:27 +0300

    Store

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, LogBox, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
@@ -6,6 +5,9 @@ import Toast from 'react-native-toast-message';
 // Redux
 import { Provider } from 'react-redux';
 import store from './Redux/store';
+
+// Context API
+import Auth from './Context/store/Auth';
 
 // Navigators
 import Main from './Navigators/Main';
@@ -18,13 +20,15 @@ LogBox.ignoreAllLogs(true);
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <Header />
-        <Main />
-        <Toast ref={(ref) => Toast.setRef(ref)} />
-      </NavigationContainer>
-    </Provider>
+    <Auth>
+      <Provider store={store}>
+        <NavigationContainer>
+          <Header />
+          <Main />
+          <Toast ref={(ref) => Toast.setRef(ref)} />
+        </NavigationContainer>
+      </Provider>
+    </Auth>
   );
 }
 
