5e7e4dfc889d5c00db1872e1ab3f8ff9633ee696
theo82 <theoaristi53@gmail.com>
2021-04-24 09:58:43 +0300

    isEmpty - Context API

--- Context/reducers/Auth.reducer.js
+++ Context/reducers/Auth.reducer.js
@@ -1,11 +1,12 @@
 import { SET_CURRENT_USER } from '../actions/Auth.actions';
+import isEmpty from '../../assets/common/is-empty';
 
 export default function (state, action) {
   switch (action.type) {
     case SET_CURRENT_USER:
       return {
         ...state,
-        isAuthenticated: [], // TODO
+        isAuthenticated: !isEmpty(action.payload), // TODO
         user: action.payload,
         userProfile: action.userProfile,
       };
