d3d545a284b7def6d6cda63b8468abc248f8d61b
theo82 <theoaristi53@gmail.com>
2021-04-25 12:57:20 +0300

    Profile Page

--- Screens/User/UserProfile.js
+++ Screens/User/UserProfile.js
@@ -1,12 +1,88 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useContext, useState, useEffect, useCallback } from 'react';
+import { View, Text, ScrollView, Button, StyleSheet } from 'react-native';
+import { Container } from 'native-base';
+import { useFocusEffect } from '@react-navigation/native';
+import AsyncStorage from '@react-native-community/async-storage';
+
+import axios from 'axios';
+import baseURL from '../../assets/common/baseUrl';
+
+import AuthGlobal from '../../Context/store/AuthGlobal';
+import { logoutUser } from '../../Context/actions/Auth.actions';
 
 const UserProfile = (props) => {
+  const context = useContext(AuthGlobal);
+  const [userProfile, setUserProfile] = useState();
+
+  useEffect(() => {
+    if (
+      context.stateUser.isAuthenticated === false ||
+      context.stateUser.isAuthenticated === null
+    ) {
+      props.navigation.navigate('Login');
+    }
+
+    AsyncStorage.getItem('jwt')
+      .then((res) => {
+        axios
+          .get(`${baseURL}users/${context.stateUser.user.userId}`, {
+            headers: { Authorization: `Bearer ${res}` },
+          })
+          .then((user) => {
+            setUserProfile(user.data);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    return () => {
+      setUserProfile();
+    };
+  }, [context.stateUser.isAuthenticated]);
+
   return (
-    <View>
-      <Text>UserProfile Screen</Text>
-    </View>
+    <Container style={styles.container}>
+      <ScrollView contentContainerStyle={styles.subContainer}>
+        <Text style={{ fontSize: 30 }}>
+          {userProfile ? userProfile.name : ''}
+        </Text>
+        <View style={{ marginTop: 20 }}>
+          <Text style={{ margin: 10 }}>
+            Email: {userProfile ? userProfile.email : ''}
+          </Text>
+          <Text style={{ margin: 10 }}>
+            Phone: {userProfile ? userProfile.phone : ''}
+          </Text>
+        </View>
+        <View style={{ marginTop: 80 }}>
+          <Button
+            title={'Sign Out'}
+            onPress={() => [
+              AsyncStorage.removeItem('jwt'),
+              logoutUser(context.dispatch),
+            ]}
+          />
+        </View>
+      </ScrollView>
+    </Container>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  subContainer: {
+    alignItems: 'center',
+    marginTop: 60,
+  },
+  order: {
+    marginTop: 20,
+    alignItems: 'center',
+    marginBottom: 60,
+  },
+});
 
 export default UserProfile;
