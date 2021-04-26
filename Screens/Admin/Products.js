90922f2995b2f0c179eafc02d0ed166f4ff49891
theo82 <theoaristi53@gmail.com>
2021-04-26 17:15:11 +0300

    Product List

--- Screens/Admin/Products.js
+++ Screens/Admin/Products.js
@@ -50,7 +50,29 @@ const Products = (props) => {
 
   return (
     <View>
-      <Text>Products Screen</Text>
+      <View>
+        <Header searchBar rounded>
+          <Item style={{ padding: 5 }}>
+            <Icon name='seach' />
+            <Input
+              placeholder='Search'
+              // onChange
+            />
+          </Item>
+        </Header>
+      </View>
+
+      {loading ? (
+        <View>
+          <ActivityIndicator size='large' color='red' />
+        </View>
+      ) : (
+        <FlatList
+          data={productFilter}
+          renderItem={({ item, index }) => <Text>{item.name}</Text>}
+          keyExtractor={(item) => item.id}
+        />
+      )}
     </View>
   );
 };
